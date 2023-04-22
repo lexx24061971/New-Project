c1960381037a08a54a9df088d0cc66773cdaa17c
lexx24061971 <130874209+lexx24061971@users.noreply.github.com>
2023-04-22 16:40:46 +0400

    Initial Commit

---
+++ New Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  New Project
+//
+//  Created by Алексей Налимов on 22.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
